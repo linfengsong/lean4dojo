64011a5655df887b75ecd9633f317e9a355e8455
linfengsong <34211305+linfengsong@users.noreply.github.com>
2025-12-30 06:52:49 -0500

    Add files via upload

---
+++ Lean4dojo.lean
@@ -0,0 +1,9 @@
+-- This module serves as the root of the `lean4dojo` library.
+-- Import modules here that should be built as part of the library.
+
+import Lean4dojo.Basic
+import Lean4dojo.Share
+import Lean4dojo.Pp
+import Lean4dojo.Path
+import Lean4dojo.Traversal
+import Lean4dojo.ExtractData
---
+++ lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+
+open Lake DSL
+
+package lean4dojo where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Lean4dojo» where
+  -- add library configuration options here
