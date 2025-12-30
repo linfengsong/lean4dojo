b419d9966f4ef3121392938573945cc50fe763d7
linfengsong <34211305+linfengsong@users.noreply.github.com>
2025-12-30 17:55:36 -0500

    Update logging in processAllFile function

--- Lean4dojo/ExtractData.lean
+++ Lean4dojo/ExtractData.lean
@@ -105,8 +105,7 @@ def shouldProcess (path : FilePath) (noDeps : Bool) : IO Bool := do
 Trace all *.lean files in the current directory whose corresponding *.olean file exists.
 -/
 def processAllFile (extractLeanPath : String) (noDeps : Bool) : IO Unit := do
-  IO.println s!"processAllFiles, noDeps, {noDeps}!"
-  IO.println s!"processAllFiles, {hello}!"
+  IO.println s!"processAllFiles, extractLeanPath: {extractLeanPath}, noDeps: {noDeps}!"
   let cwd ← IO.currentDir
   assert! cwd.fileName != "lean4"
   println! "Extracting data at {cwd}"
