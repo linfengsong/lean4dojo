7ab1ab2340ce4789e14accbfbeb9708b9fcb6653
linfengsong <34211305+linfengsong@users.noreply.github.com>
2025-12-30 17:49:06 -0500

    Remove import of Lean4dojo.Basic module

--- Lean4dojo.lean
+++ Lean4dojo.lean
@@ -1,7 +1,6 @@
 -- This module serves as the root of the `lean4dojo` library.
 -- Import modules here that should be built as part of the library.
 
-import Lean4dojo.Basic
 import Lean4dojo.Share
 import Lean4dojo.Pp
 import Lean4dojo.Path
